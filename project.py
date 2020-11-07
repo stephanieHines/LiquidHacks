2151bdd78a429eb7a4155323a577373a4155c758
mikeyeunglol <73412330+mikeyeunglol@users.noreply.github.com>
2020-11-07 18:39:29 -0500

    Add files via upload

---
+++ project.py
@@ -0,0 +1,23 @@
+print("Hi there welcome! Here you'll learn more"
+      " about yourself through this lego legends quiz.")
+
+x = input("What role do you play?: \nType (a) for top\nType (b) for jungle"
+          "\nType (c) for mid\nType (d) for adc\nType (e) for support\n")
+
+if (x=="a"):
+    print("You're a lone wolf")
+
+elif (x=="b"):
+    print("You're a masochist")
+
+elif (x=="c"):
+    print("You're a control freak")
+
+elif (x=="d"):
+    print("You're a Star")
+
+elif (x=="e"):
+    print("You're a babysitter")
+
+else:
+    print("Read the directions, this is why you're iron -_-")
