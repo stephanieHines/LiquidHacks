0b067341312ed351547aaf49a9a27f1edfa8661d
marquini76 <derekm2@uci.edu>
2020-11-07 16:50:48 -0800

    Add files via upload

--- WinQueue_UI.py
+++ WinQueue_UI.py
@@ -68,7 +68,8 @@ class WinQueueUI:
             sticky = tkinter.S)
 
         self.ok_button = tkinter.Button(
-            master = settings_frame, text = 'OK', font = DEFAULT_FONT)
+            master = settings_frame, text = 'OK', font = DEFAULT_FONT,
+            command = self._ok_button)
 
         self.ok_button.grid(row = 2, column = 0, padx = 10, pady = 10,
             sticky = tkinter.E + tkinter.W + tkinter.N + tkinter.S)
@@ -82,6 +83,7 @@ class WinQueueUI:
       
         canvas_width = self._canvas.winfo_width()
         canvas_height = self._canvas.winfo_height()
+        self.ig = None
 
     def run(self) -> None:
         self._root_window.mainloop()
@@ -89,5 +91,113 @@ class WinQueueUI:
     def _cancel_button(self):
         self._root_window.destroy()
 
+    def get_ig(self) -> int:
+        return self.ig
+
+    def _ok_button(self):
+        self.ig = self._ig_entry.get()
+        self._root_window.destroy()
+
+class QuizUI:
+    def __init__(self, ig):
+        self._quiz_window = tkinter.Tk()
+        self.top = tkinter.PhotoImage(master = self._quiz_window, file = "DariusSquare.gif")
+        self.jungle = tkinter.PhotoImage(master = self._quiz_window, file = "Lee-SinSquare.gif")
+        self.mid = tkinter.PhotoImage(master = self._quiz_window, file = "ZedSquare.gif")
+        self.adc = tkinter.PhotoImage(master = self._quiz_window, file = "AsheSquare.gif")
+        self.supp = tkinter.PhotoImage(master = self._quiz_window, file = "DJSonaSquare.gif")
+
+        self._canvas = tkinter.Canvas(
+            master = self._quiz_window, width = 400, height = 500)
+
+        title_label = tkinter.Label(
+            master = self._quiz_window, text = "Hello {}! What role do you play?".format(ig),
+            font = DEFAULT_FONT)
+
+        title_label.grid(
+            row = 0, column = 0, padx = 10, pady = 10,
+            sticky = tkinter.N)
+
+        choice_frame = tkinter.Frame(master = self._quiz_window)
+
+        choice_frame.grid(
+            row = 1, column = 0, padx = 10, pady = 10,
+            sticky = tkinter.S)
+        
+        self.top_button = tkinter.Button(
+            master = choice_frame, text = 'Top Lane', font = DEFAULT_FONT)
+        
+        self.top_button.grid(row = 0, column = 0, padx = 10, pady = 10,
+            sticky = tkinter.W + tkinter.E)
+
+        top_label = tkinter.Label(
+            master = choice_frame, image = self.top)
+
+        top_label.grid(
+            row = 0, column = 1, padx = 10, pady = 10,
+            sticky = tkinter.E)
+
+        self.jungle_button = tkinter.Button(
+            master = choice_frame, text = 'Jungle', font = DEFAULT_FONT)
+        
+        self.jungle_button.grid(row = 1, column = 0, padx = 10, pady = 10,
+            sticky = tkinter.W + tkinter.E)
+
+        jungle_label = tkinter.Label(
+            master = choice_frame, image = self.jungle)
+
+        jungle_label.grid(
+            row = 1, column = 1, padx = 10, pady = 10,
+            sticky = tkinter.E)
+
+        self.mid_button = tkinter.Button(
+            master = choice_frame, text = 'Mid Lane', font = DEFAULT_FONT)
+        
+        self.mid_button.grid(row = 2, column = 0, padx = 10, pady = 10,
+            sticky = tkinter.W + tkinter.E)
+
+        mid_label = tkinter.Label(
+            master = choice_frame, image = self.mid)
+
+        mid_label.grid(
+            row = 2, column = 1, padx = 10, pady = 10,
+            sticky = tkinter.E)
+
+        self.adc_button = tkinter.Button(
+            master = choice_frame, text = 'Bot Lane', font = DEFAULT_FONT)
+        
+        self.adc_button.grid(row = 0, column = 2, padx = 10, pady = 10,
+            sticky = tkinter.W + tkinter.E)
+
+        adc_label = tkinter.Label(
+            master = choice_frame, image = self.adc)
+
+        adc_label.grid(
+            row = 0, column = 3, padx = 10, pady = 10,
+            sticky = tkinter.E)
+
+        self.support_button = tkinter.Button(
+            master = choice_frame, text = 'Support', font = DEFAULT_FONT)
+        
+        self.support_button.grid(row = 1, column = 2, padx = 10, pady = 10,
+            sticky = tkinter.W + tkinter.E)
+
+        support_label = tkinter.Label(
+            master = choice_frame, image = self.supp)
+
+        support_label.grid(
+            row = 1, column = 3, padx = 10, pady = 10,
+            sticky = tkinter.E)
+        
+        canvas_width = self._canvas.winfo_width()
+        canvas_height = self._canvas.winfo_height()
+        
+    def run(self) -> None:
+        self._quiz_window.mainloop()
+
 if __name__ == '__main__':
-    WinQueueUI().run()
+    winqueue = WinQueueUI()
+    winqueue.run()
+    player_ig = winqueue.get_ig()
+    QuizUI(player_ig).run()
+    
