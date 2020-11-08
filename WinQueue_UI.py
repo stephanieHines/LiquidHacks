f0cd711d5b2d14b7c3c0038a19df1d8685ab3ba4
marquini76 <derekm2@uci.edu>
2020-11-07 23:58:01 -0800

    Add files via upload

--- WinQueue_UI.py
+++ WinQueue_UI.py
@@ -102,88 +102,98 @@ class QuizUI:
     def __init__(self, ig):
         self._quiz_window = tkinter.Tk()
         self.top = tkinter.PhotoImage(master = self._quiz_window, file = "DariusSquare.gif")
+        self.top_emote = tkinter.PhotoImage(master = self._quiz_window, file = "Youre-Next-Emote.gif")
         self.jungle = tkinter.PhotoImage(master = self._quiz_window, file = "Lee-SinSquare.gif")
+        self.jungle_emote = tkinter.PhotoImage(master = self._quiz_window, file = "Yikes-Emote.gif")
         self.mid = tkinter.PhotoImage(master = self._quiz_window, file = "ZedSquare.gif")
+        self.mid_emote = tkinter.PhotoImage(master = self._quiz_window, file = "Tough-Times-Emote.gif")
         self.adc = tkinter.PhotoImage(master = self._quiz_window, file = "AsheSquare.gif")
+        self.adc_emote = tkinter.PhotoImage(master = self._quiz_window, file = "Adoeable-Emote.gif")
         self.supp = tkinter.PhotoImage(master = self._quiz_window, file = "DJSonaSquare.gif")
+        self.supp_emote = tkinter.PhotoImage(master = self._quiz_window, file = "Are-You-Serious-Emote.gif")
 
         self._canvas = tkinter.Canvas(
             master = self._quiz_window, width = 400, height = 500)
 
-        title_label = tkinter.Label(
+        self.title_label = tkinter.Label(
             master = self._quiz_window, text = "Hello {}! What role do you play?".format(ig),
             font = DEFAULT_FONT)
 
-        title_label.grid(
+        self.title_label.grid(
             row = 0, column = 0, padx = 10, pady = 10,
             sticky = tkinter.N)
 
-        choice_frame = tkinter.Frame(master = self._quiz_window)
+        self.choice_frame = tkinter.Frame(master = self._quiz_window)
 
-        choice_frame.grid(
+        self.choice_frame.grid(
             row = 1, column = 0, padx = 10, pady = 10,
             sticky = tkinter.S)
         
         self.top_button = tkinter.Button(
-            master = choice_frame, text = 'Top Lane', font = DEFAULT_FONT)
+            master = self.choice_frame, text = 'Top Lane', font = DEFAULT_FONT,
+            command = self.toplane_command)
         
         self.top_button.grid(row = 0, column = 0, padx = 10, pady = 10,
             sticky = tkinter.W + tkinter.E)
 
         top_label = tkinter.Label(
-            master = choice_frame, image = self.top)
+            master = self.choice_frame, image = self.top)
 
         top_label.grid(
             row = 0, column = 1, padx = 10, pady = 10,
             sticky = tkinter.E)
 
         self.jungle_button = tkinter.Button(
-            master = choice_frame, text = 'Jungle', font = DEFAULT_FONT)
+            master = self.choice_frame, text = 'Jungle', font = DEFAULT_FONT,
+            command = self.jungle_command)
         
         self.jungle_button.grid(row = 1, column = 0, padx = 10, pady = 10,
             sticky = tkinter.W + tkinter.E)
 
         jungle_label = tkinter.Label(
-            master = choice_frame, image = self.jungle)
+            master = self.choice_frame, image = self.jungle)
 
         jungle_label.grid(
             row = 1, column = 1, padx = 10, pady = 10,
             sticky = tkinter.E)
 
         self.mid_button = tkinter.Button(
-            master = choice_frame, text = 'Mid Lane', font = DEFAULT_FONT)
+            master = self.choice_frame, text = 'Mid Lane', font = DEFAULT_FONT,
+            command = self.midlane_command)
         
         self.mid_button.grid(row = 2, column = 0, padx = 10, pady = 10,
             sticky = tkinter.W + tkinter.E)
 
         mid_label = tkinter.Label(
-            master = choice_frame, image = self.mid)
+            master = self.choice_frame, image = self.mid)
 
         mid_label.grid(
             row = 2, column = 1, padx = 10, pady = 10,
             sticky = tkinter.E)
 
         self.adc_button = tkinter.Button(
-            master = choice_frame, text = 'Bot Lane', font = DEFAULT_FONT)
+            master = self.choice_frame, text = 'Bot Lane', font = DEFAULT_FONT,
+            command = self.botlane_command)
         
         self.adc_button.grid(row = 0, column = 2, padx = 10, pady = 10,
             sticky = tkinter.W + tkinter.E)
 
         adc_label = tkinter.Label(
-            master = choice_frame, image = self.adc)
+            master = self.choice_frame, image = self.adc)
 
         adc_label.grid(
             row = 0, column = 3, padx = 10, pady = 10,
             sticky = tkinter.E)
 
         self.support_button = tkinter.Button(
-            master = choice_frame, text = 'Support', font = DEFAULT_FONT)
+            master = self.choice_frame, text = 'Support', font = DEFAULT_FONT,
+            command = self.support_command)
         
         self.support_button.grid(row = 1, column = 2, padx = 10, pady = 10,
             sticky = tkinter.W + tkinter.E)
 
         support_label = tkinter.Label(
-            master = choice_frame, image = self.supp)
+            master = self.choice_frame, image = self.supp)
 
         support_label.grid(
             row = 1, column = 3, padx = 10, pady = 10,
@@ -194,6 +204,52 @@ class QuizUI:
         
     def run(self) -> None:
         self._quiz_window.mainloop()
+
+    def toplane_command(self):
+        self.choice_frame.destroy()
+        self.title_label.config(text = "You're a lone wolf.")
+        topemote_label = tkinter.Label(
+            master = self._quiz_window, image = self.top_emote)
+        topemote_label.grid(
+            row = 1, column = 0, padx = 10, pady = 10,
+            sticky = tkinter.N + tkinter.S)
+
+    def jungle_command(self):
+        self.choice_frame.destroy()
+        self.title_label.config(text = "You're a masochist.")
+        topemote_label = tkinter.Label(
+            master = self._quiz_window, image = self.jungle_emote)
+        topemote_label.grid(
+            row = 1, column = 0, padx = 10, pady = 10,
+            sticky = tkinter.N + tkinter.S)
+
+    def midlane_command(self):
+        self.choice_frame.destroy()
+        self.title_label.config(text = "You're a control freak.")
+        topemote_label = tkinter.Label(
+            master = self._quiz_window, image = self.mid_emote)
+        topemote_label.grid(
+            row = 1, column = 0, padx = 10, pady = 10,
+            sticky = tkinter.N + tkinter.S)
+
+    def botlane_command(self):
+        self.choice_frame.destroy()
+        self.title_label.config(text = "You're a star.")
+        topemote_label = tkinter.Label(
+            master = self._quiz_window, image = self.adc_emote)
+        topemote_label.grid(
+            row = 1, column = 0, padx = 10, pady = 10,
+            sticky = tkinter.N + tkinter.S)
+
+    def support_command(self):
+        self.choice_frame.destroy()
+        self.title_label.config(text = "You're a babysitter.")
+        topemote_label = tkinter.Label(
+            master = self._quiz_window, image = self.supp_emote)
+        topemote_label.grid(
+            row = 1, column = 0, padx = 10, pady = 10,
+            sticky = tkinter.N + tkinter.S)
+        
 
 if __name__ == '__main__':
     winqueue = WinQueueUI()
